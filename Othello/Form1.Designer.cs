eab5703e1c08708eb543ea0b5eb739df90f4ff2a
Minoru Yamakawa <ymkwprg@gmail.com>
2019-04-12 00:09:56 +0900

    first commit

---
+++ Othello/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Othello
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel1.Location = new System.Drawing.Point(40, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(641, 641);
+            this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.point);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(834, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(834, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(838, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(838, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(838, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 53);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "新規ゲーム(&N)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.newGame);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(838, 479);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 53);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "終了(&E)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ExitApp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 747);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Reversi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Othello/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Othello
+{
+    public partial class Form1 : Form
+    {
+        Bitmap[] img = new Bitmap[4];
+        static int[,] bo = new int[10, 10];
+        static int cs; //Current Stone
+        static string[] color = {"","黒","白" };
+        static int[] cntStn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Left = 40;
+            panel1.Top = 40;
+            panel1.Width = 321;
+            panel1.Height = 321;
+            Width = 600;
+            Height = 450;
+            img[0] = Properties.Resources.none;
+            img[1] = Properties.Resources.black;
+            img[2] = Properties.Resources.white;
+            img[3] = Properties.Resources.set;
+            label1.Text = "新規ゲームをクリックしてください";
+            label2.Text = "";
+            label3.Text = "黒：";
+            label4.Text = "白：";
+            panel1.Enabled = false;
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            for (int i = 0; i < 9; i++)
+            {
+                g.DrawLine(Pens.Black, 0, 40 * i, 320, 40 * i);
+                g.DrawLine(Pens.Black, 40 * i, 0, 40 * i, 320);
+            }
+            for (int i = 1; i < 9; i++)
+            {
+                for (int j = 1; j < 9; j++)
+                {
+                    g.DrawImage(img[getBo(i,j)], (i - 1) * 40 + 4, (j - 1) * 40 + 4, 32, 32);//画像描写
+                }
+            }
+        }
+        static int getBo(int x, int y)
+        {
+            return bo[x, y];
+        }
+        static void setBo(int s, int x, int y)
+        {
+            bo[x, y] = s;
+        }
+        private void point(object sender, EventArgs e)
+        {
+            Point getXY = PointToClient(Control.MousePosition);
+            int x = (getXY.X) / 40;
+            int y = (getXY.Y) / 40;
+            //label5.Text = ("x=" + getXY.X.ToString() + " y=" + getXY.Y.ToString());
+            input(cs, x, y);
+            Refresh();
+        }
+        void plusCount()
+        {
+            cs = cs % 2 + 1;
+        }
+        void countStone()
+        {
+            cntStn = new int[4];
+            for(int i = 0;i< 4; i++)
+            {
+                cntStn[i] = 0;
+            }
+            for (int i = 1; i < 9;i++)
+            {
+                for (int j = 1; j < 9; j++)
+                {
+                    cntStn[bo[i,j]]++;
+                }
+
+            }
+        }
+        void input(int s, int x, int y)
+        {
+            label1.Text = ("");
+            label2.Text = ("");
+            if (getBo(x,y)==3)
+            {
+                setBo(s,x,y);
+                turn(s, x, y);
+                plusCount();//相手に渡す
+                setCand(cs);
+                countStone();
+                if (cntStn[3] == 0)//相手の置ける場所が0の場合
+                {
+                    plusCount();//味方に渡す
+                    setCand(cs);
+                    countStone();
+                    if(cntStn[3] == 0)//味方の置ける場所が0の場合
+                    {
+                        exitGame();//ゲームを終了
+                    }
+                    else
+                    {
+                        label1.Text = ("置ける場所がないのでパスします");//テキスト表示
+                        label2.Text = (color[cs] + "の番です");
+                    }
+                }
+                else
+                {
+                    label1.Text = (color[cs] + "の番です");
+                }
+            }else
+            {
+                label1.Text = ("その場所には置けません");
+                label2.Text = (color[cs] + "の番です");
+            }
+            textCount();
+        }
+        void setCand(int s)
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                for (int j = 1; j < 9; j++)
+                {
+                    if (getBo(i,j) == 0 || getBo(i, j) == 3)
+                    {
+                        if (srch(s,i,j))
+                        {
+                            setBo(3,i,j);
+                        }else
+                        {
+                            setBo(0,i,j);
+                        }
+                        
+                    }
+                }
+            }
+        }
+        bool srch(int s, int x, int y)
+        {
+            bool isBreak = false;
+            bool judge = false;
+            int cc; //current coordinate
+            for (int v = -1; v < 2; v++)
+            {
+                for (int h = -1; h < 2; h++)
+                {
+                    if (h == 0 && v == 0)
+                        continue;
+                    for (int i = 1; i < 9; i++)
+                    {
+                        cc = bo[x + h * i, y + v * i];
+                        if (cc == 0 || cc == 3)
+                        {//kuurann
+                            isBreak = true;
+                        }
+                        else if (cc == s % 2 + 1)
+                        {//enemy no iro
+                            continue;
+                        }
+                        else if (cc == s)
+                        {//mikata no iro
+                            if (i > 1)
+                            {
+                                judge = true;
+                                isBreak = true;
+                            }
+                            else
+                            {
+                                isBreak = true;
+                            }
+                        }
+                        if (isBreak)
+                            break;
+                    }
+                    if (judge)
+                        break;
+                }
+                if (judge)
+                    break;
+            }
+            return judge;
+        }
+        void turn(int s, int x, int y)
+        {
+            int cc; //current coordinate
+            for (int v = -1; v < 2; v++)
+            {
+                for (int h = -1; h < 2; h++)
+                {
+                    if (v == 0 && h == 0)
+                        continue;
+                    for (int i = 1; i < 8; i++)
+                    {
+                        cc = bo[x + h * i, y + v * i];
+                        if (cc == s && i > 1)
+                        {
+                            for (int j = 0; j < i; j++)
+                            {
+                                setBo(s, x + h * j, y + v * j);
+                            }
+                        }
+                        else if (cc == 0 || cc == 3)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        void exitGame()
+        {
+            panel1.Enabled = false;
+            if (cntStn[1]>cntStn[2])
+            {
+                label1.Text = "黒が勝ちました";
+            }
+            else if (cntStn[2]>cntStn[1])
+            {
+                label1.Text = "白が勝ちました";
+            }else
+            {
+                label1.Text = "引き分けです";
+            }
+
+        }
+        void textCount()
+        {
+            label3.Text = "黒：" + cntStn[1].ToString() ;
+            label4.Text = "白：" + cntStn[2].ToString();
+        }
+
+        private void newGame(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    setBo(0, i, j);
+                }
+            }
+            cs = 1;
+            bo[4, 4] = 1;
+            bo[5, 5] = 1;
+            bo[4, 5] = 2;
+            bo[5, 4] = 2;
+            setCand(cs);
+            countStone();
+            textCount();
+            label1.Text = (color[cs] + "の番です");
+            label2.Text = ("");
+            panel1.Enabled = true;
+            panel1.Refresh();
+        }
+
+        private void ExitApp(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+    //class Player
+    //{
+    //    int color;
+    //    int cntStn;
+
+    //}
+}
